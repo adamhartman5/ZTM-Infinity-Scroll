b22f3b6e34f075f10957a4f5192a7b795aed6a7f
Adam Hartman <adamhartman5@gmail.com>
2020-12-28 22:07:43 -0500

    add API key and create async function

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 // Unsplash API
-const apiURL = `https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY`
+const count = 10;
+const apiKey = '8ew49ZBwkpuGfF6cIiiQfhE6Y2THJIbbjgUPz5fEWuU';
+const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Get photos from Unsplash API
+async function getPhotos() {
+    try {
+        const response = await fetch(apiURL);
+        const data = await response.json();
+        console.log(data);
+
+    } catch (error) {
+        // Catch error here
+    }
+}
+
+// On Load
+getPhotos();
